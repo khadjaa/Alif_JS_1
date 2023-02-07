b0009d61b0fc9718196078fbbf032a63188b95c7
khadjaa <islomkhadja2001@icloud.com>
2023-02-08 01:23:57 +0300

    7 h/w

---
+++ DZ_7_Salom/app.js
@@ -0,0 +1,36 @@
+const partners = [
+  {
+    id: 'tobon',
+    url: 'https://salom.alif.tj/partners/tobon',
+    logoUrl: 'https://alif-skills.pro/media/tobon.svg',
+    title: 'Тобон',
+    category: 'Отопление',
+    period: 12,
+  },
+  {
+    id: 'charme',
+    url: 'https://salom.alif.tj/partners/charme',
+    logoUrl: 'https://alif-skills.pro/media/charme.svg',
+    title: 'Charme',
+    category: 'Женская одежда',
+    period: 3,
+  },
+  {
+    id: 'akika',
+    url: 'https://salom.alif.tj/partners/akika',
+    logoUrl: 'https://alif-skills.pro/media/akika.svg',
+    title: 'Ақиқа',
+    category: 'Ювелирные изделия',
+    period: 6,
+  },
+  {
+    id: 'fosila',
+    url: 'https://salom.alif.tj/partners/fosila',
+    logoUrl: 'https://alif-skills.pro/media/fosila.svg',
+    title: 'Fosila',
+    category: 'Оргтехника',
+    period: 24,
+  },
+];
+
+console.log(partners);
