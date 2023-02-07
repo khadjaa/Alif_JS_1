c543c155083e64253d67110fd8a9a1852dc6f15c
khadjaa <islomkhadja2001@icloud.com>
2023-02-07 20:31:57 +0300

    4 h/w

---
+++ DZ_4_Fun_Cash/js/app.js
@@ -0,0 +1,10 @@
+function calculateCashback(amount, percent) {
+
+    return cashback = amount * percent / 100;
+}
+
+const amount = 1000;
+const percent = 50;
+
+const result = calculateCashback(amount, percent);
+console.log(result);
